3786926f448c1b740376ca0a3839b249ce8012d8
Jon Carlos O'Neil <jco103001@gmail.com>
2023-09-07 08:48:32 -0400

    Updated about page
    got rid of template text

--- src/pages/mainPage/aboutPage/AboutPage.js
+++ src/pages/mainPage/aboutPage/AboutPage.js
@@ -7,9 +7,9 @@ function AboutPage(){
         <div className={styles.container}>
             <h4>About us</h4>
             <h6>Who We Are</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>We are some Umass Boston Students who onceupon a time looked for tutors for difficult classes and coulndt find anyone willing.</p>
             <h6>Our Mission</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>We want to provide a place to solve the problem we had. We want to make a place for Students to find help easily and reliably. From the start of their time at UMB to the end of their time at UMB. So they can have support from 100 level courses to 400 level courses </p>
             <p>~BLANK</p>
         </div>
         </div>
